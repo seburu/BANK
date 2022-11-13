dbaf4067ebc0d9df512979d146fc65f2bc883960
seburu <sebynoah@gmail.com>
2022-11-13 21:12:27 +0100

    action class

---
+++ src/action.java
@@ -0,0 +1,12 @@
+public class action {
+    private char action;
+    private String ID;
+    private int amount;
+
+    public action(char action){
+        this.action = action;
+
+    }
+
+
+}
--- src/main.java
+++ src/main.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 public class main {
-
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many customers are in the queue?");
+        int threads = input.nextInt();
 
-        for (int i = 0; i <= 3; i++){
+
+        for (int i = 0; i < threads; i++){
             MultiThread myThing = new MultiThread(i);
             myThing.start();
             //myThing.join(); Needs a try catch around it but ensures that the next thread joins when the first is dead
             //myThing.isAlive() Returns a boolean for weather the thread is still running
         }
-        //myThing.run(); This runs the code, but not in its own thread.
+        //myThing.run(); //This runs the code, but not in its own thread.
 
     }
+
+    action test = new action('w');
+
 }
 
