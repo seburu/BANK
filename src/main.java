d8387fd681bdebf1a0359193ad63c0084fe2700e
MathildeElia <91071893+MathildeElia@users.noreply.github.com>
2022-11-14 08:26:35 +0100

    InitializeAccounts method created.

--- src/main.java
+++ src/main.java
@@ -1,6 +1,20 @@
 import java.util.Scanner;
 
 public class main {
+    int noOfUsers;
+    String[] names = {"Sebastian","Marco","Sofie","Mathilde"};
+
+    public Account[] initializeAccounts(){
+        noOfUsers = names.length;
+        Account[] accounts = new Account[noOfUsers];
+
+        for(int i=0; i<noOfUsers-1;i++){
+            Account account = new Account(100,names[i]);
+            accounts[i] = account;
+        }
+        return accounts;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("How many customers are in the queue?");
