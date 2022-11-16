3c2ebede00c577e2074e9b1cbb059249e709b410
MathildeElia <91071893+MathildeElia@users.noreply.github.com>
2022-11-16 14:35:13 +0100

    Changed from boolean isInUse to mutex in account class.

--- src/Account.java
+++ src/Account.java
@@ -1,7 +1,10 @@
+import java.util.concurrent.Semaphore;
+
 public class Account {
     int balance;
     String name;
-    boolean isInUse = false;
+    //boolean isInUse = false;
+    Semaphore mutex = new Semaphore(1);
 
     public Account(int balance, String name){
         this.balance = balance;
@@ -10,7 +13,7 @@ public class Account {
 
     public Boolean changeBalance(int amount){
         if (this.balance + amount < 0){
-            System.out.println("You do not have enough money to withdraw this amount");
+            System.out.println(name + " do not have enough money to withdraw this amount");
             return false;
         }
         else{
